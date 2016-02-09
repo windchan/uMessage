e5b78d07b496b10c3a2cdfe3b3a79fa853198604
Qiang(Andrew) Yu <Andrew_yq@hotmail.com>
2016-02-09 01:24:16 -0800

    easy

--- src/p2/sorts/QuickSort.java
+++ src/p2/sorts/QuickSort.java
@@ -10,6 +10,35 @@ public class QuickSort {
     }
 
     public static <E> void sort(E[] array, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        int lo = 0;
+        int hi = array.length - 1;
+        sort(array, lo, hi, comparator);
+    }
+    
+    private static <E> void sort(E[] array, int lo, int hi, Comparator<E> comparator) { 
+        if(lo < hi) {
+            int i = lo;
+            int front = lo + 1;
+            int end = hi;
+            E element = array[i];
+            while(front <= end) {
+                if(comparator.compare(array[front], element) >= 0) {
+                    swap(array, front, end);
+                    end--;
+                } else {
+                    swap(array, front, i);
+                    front++;
+                    i++;
+                }
+            }
+            sort(array, lo, i, comparator);
+            sort(array, i + 1, hi, comparator);
+        }       
+    }
+    
+    private static <E> void swap(E[] array, int a, int b) {
+        E temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
     }
 }
