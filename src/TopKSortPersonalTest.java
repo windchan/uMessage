b7cbe8285a3672cf04f24749d583179eba0e4c24
Qiang(Andrew) Yu <Andrew_yq@hotmail.com>
2016-02-09 02:15:30 -0800

    TopKSort easy

---
+++ src/TopKSortPersonalTest.java
@@ -0,0 +1,46 @@
+import java.util.Comparator;
+
+import p2.sorts.TopKSort;
+
+public class TopKSortPersonalTest {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print(integer_sorted());
+        System.out.print(integer_random());
+    }
+    
+    public static int integer_sorted() {
+        int K = 4;
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer[] arr_sorted = {7, 8, 9, 10};
+        TopKSort.sort(arr, K, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer e1, Integer e2) {
+                return e1.compareTo(e2);
+            }
+        });
+        for(int i = 0; i < K; i++) {
+            if(!arr[i].equals(arr_sorted[i]))
+                return 0;
+        }
+        return 1;
+    }
+
+    public static int integer_random() {
+        int K = 4;
+        Integer[] arr = {3, 1, 4, 5, 9, 2, 6, 7, 8};
+        Integer[] arr_sorted = {6, 7, 8, 9};
+        TopKSort.sort(arr, K, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer e1, Integer e2) {
+                return e1.compareTo(e2);
+            }
+        });
+        for(int i = 0; i < K; i++) {
+            if(!arr[i].equals(arr_sorted[i]))
+                return 0;
+        }
+        return 1;
+    }
+}
--- src/p2/sorts/TopKSort.java
+++ src/p2/sorts/TopKSort.java
@@ -2,6 +2,7 @@ package p2.sorts;
 
 import java.util.Comparator;
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 public class TopKSort {
     public static <E extends Comparable<E>> void sort(E[] array, int k) {
@@ -9,6 +10,22 @@ public class TopKSort {
     }
 
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap<E> heap = new MinFourHeap<E>(comparator);
+        for(int i = 0; i < array.length; i++) {
+            if(heap.size() >= k) {
+                if(comparator.compare(heap.peek(), array[i]) < 0) {
+                    heap.next();
+                    heap.add(array[i]);
+                }
+            } else {
+                heap.add(array[i]);
+            }
+        }
+        for(int i = 0; i < k; i++) {
+            array[i] = heap.next();
+        }
+        for(int i = k; i < array.length; i++) {
+            array[i] = null;
+        }
     }
 }
