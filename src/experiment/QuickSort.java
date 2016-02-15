5ed639232004d11fa81058a07866714709bdc1b1
chenq9 <qiuchen95@hotmail.com>
2016-02-14 22:54:48 -0800

    Add quick sort experiment

---
+++ src/experiment/QuickSort.java
@@ -0,0 +1,65 @@
+package experiment;
+
+import java.util.Comparator;
+
+import cse332.exceptions.NotYetImplementedException;
+
+public class QuickSort {
+    public static <E extends Comparable<E>> int sort(E[] array) {
+        return QuickSort.sort(array, (x, y) -> x.compareTo(y));
+    }
+
+    public static <E> int sort(E[] array, Comparator<E> comparator) {
+        int lo = 0;
+        int hi = array.length - 1;
+        return sort(array, lo, hi, comparator);
+    }
+    
+    private static <E> int sort(E[] array, int lo, int hi, Comparator<E> comparator) { 
+        if(lo < hi) {
+            int steps = 0;
+            int i = findMedian(array, lo, hi - 1, lo + (hi - 1 - lo) / 2, comparator);
+            steps++;
+            int front = lo + 1;
+            int end = hi;
+            swap(array, lo, i);
+            steps++;
+            E pivot = array[lo];
+            i = lo;
+            while(front <= end) {
+                if(comparator.compare(array[front], pivot) >= 0) {
+                    swap(array, front, end);
+                    end--;
+                } else {
+                    swap(array, front, i);
+                    front++;
+                    i++;
+                }
+                steps++;
+            }
+            return sort(array, lo, i, comparator) + 
+            sort(array, i + 1, hi, comparator) + steps;
+        }       
+        return 0;
+    }
+    
+    private static <E> int findMedian(E[] arr, int a, int b, int c, Comparator<E> comparator) {
+        if(checkMedian(arr[a], arr[b], arr[c], comparator)) {
+            return b;
+        } else if (checkMedian(arr[b], arr[a], arr[c], comparator)) {
+            return a;
+        } else {
+            return c;
+        }
+    }
+    
+    private static <E> boolean checkMedian(E a, E b, E c, Comparator<E> comparator) {
+        return (comparator.compare(a, b) < 0 && comparator.compare(b, c) < 0) ||
+        (comparator.compare(c, b) < 0 && comparator.compare(b, a) < 0);
+    }
+    private static <E> void swap(E[] array, int a, int b) {
+        E temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
--- src/experiment/sortExperiment.java
+++ src/experiment/sortExperiment.java
@@ -4,16 +4,23 @@ import java.util.Random;
 
 public class sortExperiment {
 
-    private static final int SIZE = 50000;
+    private static final int SIZE = 500000;
     private static final Random rand = new Random();
     
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
         Integer[] nums = new Integer[SIZE];
+        //heap sort
         for (int i = 0; i < nums.length; i++) {
-            nums[i] = rand.nextInt();
+            nums[i] = rand.nextInt(10000);
         }
         System.out.println("heap sort steps: " + HeapSort.sort(nums));
+        
+        // quick sort 
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = rand.nextInt(10000);
+        }
+        System.out.println("quick sort steps: " + QuickSort.sort(nums));
     }
 
 }
